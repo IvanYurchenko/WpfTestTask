2263d64de3187877bfbb767ae6c1d35eaab0eefe
Ivan Yurchenko <ivan_yurchenko1@epam.com>
2015-10-04 12:20:00 +0300

    Basic validation added

--- WpfTestTask/Models/RowModel.cs
+++ WpfTestTask/Models/RowModel.cs
@@ -1,15 +1,127 @@
-﻿using WpfTestTask.Enums;
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using WpfTestTask.Annotations;
+using WpfTestTask.Enums;
 
 namespace WpfTestTask.Models
 {
-	public class RowModel
+	public class RowModel : IDataErrorInfo, INotifyPropertyChanged
 	{
-		public int Id { get; set; }
+		private static readonly Regex DescriptionRegex = new Regex(@"^[A-Za-z0-9 ]+$");
 
-		public string Description { get; set; }
+		private int _id;
 
-		public State State { get; set; }
+		private string _description;
 
-		public bool IsCompleted { get; set; }
+		private State _state;
+
+		private bool _isCompleted;
+
+		public int Id
+		{
+			get
+			{
+				return _id;
+			}
+			set
+			{
+				_id = value;
+				NotifyErrorChanged();
+			}
+		}
+
+		public string Description
+		{
+			get
+			{
+				return _description;
+			}
+			set
+			{
+				_description = value;
+				NotifyErrorChanged();
+			}
+		}
+
+		public State State
+		{
+			get
+			{
+				return _state;
+			}
+			set
+			{
+				_state = value;
+				NotifyErrorChanged();
+			}
+		}
+
+		public bool IsCompleted
+		{
+			get
+			{
+				return _isCompleted;
+			}
+			set
+			{
+				_isCompleted = value;
+				NotifyErrorChanged();
+			}
+		}
+
+		public string Error
+		{
+			get
+			{
+				StringBuilder sb = new StringBuilder();
+
+				PropertyDescriptorCollection props = TypeDescriptor.GetProperties(this);
+				foreach (PropertyDescriptor prop in props)
+				{
+					string propertyError = this[prop.Name];
+					if (!string.IsNullOrEmpty(propertyError))
+					{
+						sb.Append((sb.Length != 0 ? ", " : "") + propertyError);
+					}
+				}
+
+				return sb.Length == 0 ? null : sb.ToString();
+			}
+		}
+
+		public string this[string columnName]
+		{
+			get
+			{
+				if (columnName == "Description")
+				{
+					if (string.IsNullOrEmpty(Description.Trim()))
+						return "Description is required";
+
+					if (!DescriptionRegex.Match(Description).Success)
+						return "Description may only contain characters, numbers or spaces";
+				}
+
+				return "";
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private void NotifyPropertyChanged(string property)
+		{
+			if (PropertyChanged != null)
+			{
+				PropertyChanged(this, new PropertyChangedEventArgs(property));
+			}
+		}
+
+		private void NotifyErrorChanged()
+		{
+			NotifyPropertyChanged("Error");
+		}
 	}
 }
